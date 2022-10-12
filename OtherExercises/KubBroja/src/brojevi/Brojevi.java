2d26446bdf52f7920569e6b3fbba7d604ab8c5e6
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-10-12 22:30:38 +0300

    Add files via upload

---
+++ OtherExercises/KubBroja/src/brojevi/Brojevi.java
@@ -0,0 +1,18 @@
+package brojevi;
+import java.util.Scanner;
+public class Brojevi {
+
+    public static int kub(int a){
+        int kub = a*a*a;
+        return kub;
+    }
+    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Unesite jedan ceo broj: ");
+        int a = s.nextInt();
+        System.out.println("Kub tog broja  je: " +kub(a));
+                
+    }
+    
+}
