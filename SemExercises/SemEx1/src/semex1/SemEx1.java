7f21025710eecd4308b8739b3558e9e1ff7ac7fc
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-10-12 21:45:35 +0300

    Add files via upload

---
+++ SemExercises/SemEx1/src/semex1/SemEx1.java
@@ -0,0 +1,16 @@
+package semex1;
+import java.util.Scanner;
+public class SemEx1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = s.nextLine();
+        System.out.println("Enter your facultity number: ");
+        int number = s.nextInt();
+        System.out.println("Enter your age: ");
+        int age = s.nextInt();
+        System.out.println("Your name is "+name+", with faculty number "+number+" and you are "+age+" year old.");
+        
+    }
+    
+}
