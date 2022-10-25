6a8a7e55ee8f0e7ab8c05ea24373457049ae8535
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-10-25 15:56:48 +0300

    Update MyFirstProject.java

--- LabExercises/LabUpr1/myFirstProject/src/myfirstproject/MyFirstProject.java
+++ LabExercises/LabUpr1/myFirstProject/src/myfirstproject/MyFirstProject.java
@@ -7,29 +7,29 @@ public class MyFirstProject {
         //В main() метода да се прочетат име, години и рожденна дата на даден човек, въведени от клавиатурата, 
         //чрез използване на класа Scanner. Данните да се отпечатат на стандартния изход.
         
-//    Scanner s = new Scanner(System.in);
-//    System.out.println("Enter your name: ");
-//    String name = s.nextLine();
-//    System.out.println("Enter your age: ");
-//    int age = s.nextInt();
-//    System.out.println("Enter date of birth: ");
-//    String date = s.next();
-//    System.out.println(name);
-//    System.out.println(age);
-//    System.out.println(date);
+   Scanner s = new Scanner(System.in);
+   System.out.println("Enter your name: ");
+   String name = s.nextLine();
+   System.out.println("Enter your age: ");
+   int age = s.nextInt();
+   System.out.println("Enter date of birth: ");
+   String date = s.next();
+   System.out.println(name);
+   System.out.println(age);
+   System.out.println(date);
     
     
     //2. Напишете програма, която по зададени две страни на правоъгълник изчислява и отпечатва неговото лице. 
     //Дължините на страните се въвеждат от стандартния вход.
     
    
-//    Scanner s = new Scanner(System.in);
-//    System.out.println("Enter the side length of the rectangle: ");
-//    float length = s.nextFloat();
-//    System.out.println("Enter the side width of the rectangle: ");
-//    float width = s.nextFloat();
-//    float area = length * width;
-//    System.out.println("Area of rectangle is: "+Math.ceil(area));
+   Scanner s = new Scanner(System.in);
+   System.out.println("Enter the side length of the rectangle: ");
+   float length = s.nextFloat();
+   System.out.println("Enter the side width of the rectangle: ");
+   float width = s.nextFloat();
+   float area = length * width;
+   System.out.println("Area of rectangle is: "+Math.ceil(area));
     
     //3.  Напишете програма която получава от стандартния вход 5 низа, разделени с интервал и записва низовете в масив. 
     //Отпечатва низовете на екрана.
@@ -42,4 +42,3 @@ public class MyFirstProject {
     
 }
     }
-
