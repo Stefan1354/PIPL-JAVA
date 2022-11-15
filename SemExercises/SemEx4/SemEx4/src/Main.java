737f305524f17692d8ef5e130fb08188520dd062
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-11-15 22:55:19 +0200

    Add files via upload

---
+++ SemExercises/SemEx4/SemEx4/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //1. Given an array of integers find the biggest integer.
+
+//        int[] array = {24, 4, 7, 8, 10, 15};
+//        int max = array[0];
+//
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] > array[0]) {
+//                max = array[i];
+//            }
+//        }
+//        System.out.println("Max element is: " + max);
+//    }
+
+        // 2. Given an array of integers find the smallest integer.
+
+        int[] array = {-24, -4, -7, -8, -10, -15};
+        int min = array[0];
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < array[0]) {
+                min = array[0];
+            }
+        }
+        System.out.println("Min element is: " + min);
+    }
+}
